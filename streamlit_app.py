ea799a5b2b7ce12b12dbe9e5e4068218ede86ca1
ankita61299 <73284689+ankita61299@users.noreply.github.com>
2022-09-19 12:58:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,5 @@ streamlit.title('my parents  new healthy dinner')
 
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
